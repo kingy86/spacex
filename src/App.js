26d80bb21042b9a7d06a368492146153ef571467
Mark <nirvanafoo86@hotmail.com>
2020-12-12 09:58:53 +0000

    inital commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MainContainer from "./Containers/MainContainer/MainContainer";
+
+function App() {
+  return (
+    <div>
+      <MainContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/MainContainer/MainContainer.js
@@ -0,0 +1,10 @@
+import React, { Component } from " react";
+import Aux from "../../HOC/Aux";
+
+class MainContainer extends Component {
+  render() {
+    return <Aux></Aux>;
+  }
+}
+
+export default MainContainer;
