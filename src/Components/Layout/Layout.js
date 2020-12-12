0bfd803b829444d6d6cc9e385ef7aeb95d9b8b11
Mark <nirvanafoo86@hotmail.com>
2020-12-12 18:36:57 +0000

    added rocket image and header / layout styles

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,5 +1,12 @@
 import React from "react";
+import RocketImage from "../RocketImage/RocketImage";
+import "react-bulma-components/dist/react-bulma-components.min.css";
+import "./Layout.css";
 
-const Layout = (props) => <div></div>;
+const Layout = () => (
+  <div className="is-half layout">
+    <RocketImage />
+  </div>
+);
 
 export default Layout;
---
+++ src/Components/RocketImage/RocketImage.js
@@ -0,0 +1,6 @@
+import React from "react";
+import rocketImage from "../../assets/img/launch-home.png";
+
+const RocketImage = () => <img src={rocketImage} alt="Rocket Image" />;
+
+export default RocketImage;
