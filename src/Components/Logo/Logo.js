4c6053b21cb23bf90f8f19d4d0fcdbbeefa12baa
Mark <nirvanafoo86@hotmail.com>
2020-12-12 10:41:05 +0000

    adding elements to Logo component

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,10 @@
+import React from "react";
+import spaceXLogo from "../../../assets/spacex-logo.png";
+
+const Logo = () => (
+  <div className="logo">
+    <img src={spaceXLogo} alt="SpaceXLogo" />
+  </div>
+);
+
+export default Logo;
