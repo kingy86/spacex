65f2bf0c1986c2d1a1c60081bba2b4938aa1a9bf
Mark <nirvanafoo86@hotmail.com>
2020-12-13 13:16:26 +0000

    moved spacexapi to main container

--- src/Components/LaunchFeature/LaunchFeature.js
+++ src/Components/LaunchFeature/LaunchFeature.js
@@ -2,18 +2,10 @@ import React from "react";
 import "./LaunchFeature.css";
 import Filter from "../Filter/Filter";
 
-let SpacexApiWrapper = require("spacex-api-wrapper");
-
-const LaunchFeature = () => {
-  SpacexApiWrapper.getAllLaunches().then((data) => {
-    console.log(data);
-  });
-
-  return (
-    <div className="launch-feature">
-      <Filter />
-    </div>
-  );
-};
+const LaunchFeature = () => (
+  <div className="launch-feature">
+    <Filter />
+  </div>
+);
 
 export default LaunchFeature;
--- src/Containers/MainContainer/MainContainer.js
+++ src/Containers/MainContainer/MainContainer.js
@@ -3,8 +3,18 @@ import Aux from "../../HOC/Aux";
 import Header from "../../Components/Header/Header";
 import Layout from "../../Components/Layout/Layout";
 
+let SpacexApiWrapper = require("spacex-api-wrapper");
+
 class MainContainer extends Component {
   render() {
+    SpacexApiWrapper.getAllLaunches()
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log("Error ", error);
+      });
+
     return (
       <Aux>
         <Header />
