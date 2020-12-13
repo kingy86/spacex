088ab30ed915443fbbdb1978f8c92d5afa5fc061
Mark <nirvanafoo86@hotmail.com>
2020-12-13 20:25:14 +0000

    changed MainContainer to a functional Component

--- src/Containers/MainContainer/MainContainer.js
+++ src/Containers/MainContainer/MainContainer.js
@@ -1,29 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import { Container } from "react-bulma-components";
 import "react-bulma-components/dist/react-bulma-components.min.css";
 import Header from "../../Components/Header/Header";
 import Layout from "../../Components/Layout/Layout";
 
-import { SpaceX } from "spacex-api";
-
-class MainContainer extends Component {
-  render() {
-    let state = {
-      spaceLaunches: [],
-    };
-
-    SpaceX.getAllLaunches().then((data) =>
-      data.forEach((launch) => {
-        this.setState({ spaceLaunches: launch });
-      })
-    );
-    return (
-      <Container>
-        <Header />
-        <Layout />
-      </Container>
-    );
-  }
-}
+const MainContainer = () => (
+  <Container>
+    <Header />
+    <Layout />
+  </Container>
+);
 
 export default MainContainer;
