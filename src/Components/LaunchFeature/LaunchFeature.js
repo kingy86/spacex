9dceb65429fc63c62638c4d145a6b5d2b7f68407
Mark <nirvanafoo86@hotmail.com>
2020-12-13 12:10:22 +0000

    adding spacex api dependencie

--- src/Components/LaunchFeature/LaunchFeature.js
+++ src/Components/LaunchFeature/LaunchFeature.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./LaunchFeature.css";
-
 import Filter from "../Filter/Filter";
 
-const LaunchFeature = (props) => (
-  <div className="launch-feature">
-    <Filter />
-  </div>
-);
+let SpacexApiWrapper = require("spacex-api-wrapper");
+
+const LaunchFeature = () => {
+  SpacexApiWrapper.getAllLaunches().then((data) => {
+    console.log(data);
+  });
+
+  return (
+    <div className="launch-feature">
+      <Filter />
+    </div>
+  );
+};
 
 export default LaunchFeature;
