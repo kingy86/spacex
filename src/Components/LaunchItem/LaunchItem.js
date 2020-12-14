0fdcf1fce111d26e0914a7fe5c9f41bfc1ff2125
Mark <nirvanafoo86@hotmail.com>
2020-12-14 22:04:36 +0000

    styled launch item with box shadow

--- src/Components/LaunchItem/LaunchItem.js
+++ src/Components/LaunchItem/LaunchItem.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Columns } from "react-bulma-components";
+import "./LaunchItem.css";
 
 const LaunchItem = (props) => (
   <div class="launch-item">
     <Columns>
-      <Columns.Column>
+      <Columns.Column size="two-thirds" className="launch-title">
         <h1>#{props.flightNumber}</h1>
         <h1>{props.missionName}</h1>
       </Columns.Column>
